2410c49ad047f73bbb488aca30ced8b10acec499
Anant <amishra1577@gmail.com>
2022-07-01 16:41:18 +0530

    Created readme and changed algo

--- color_transfer.py
+++ color_transfer.py
@@ -2,60 +2,42 @@ import cv2
 import numpy as np
 import argparse
 
-def mean_std(image):
-	# Splitting image into channels
-	c1, c2, c3 = cv2.split(image)
-
-	# Calculating Mean and Standard Deviation of each channel
-	c1mean, c1std = c1.mean(), c1.std()
-	c2mean, c2std = c2.mean(), c2.std()
-	c3mean, c3std = c3.mean(), c3.std()
-	return (c1mean, c2mean, c3mean, c1std, c2std, c3std)
-
 
 def color_transfer(source, target):
-	# Converting image from RGB to Lαβ color space
 	source = cv2.cvtColor(source, cv2.COLOR_BGR2LAB).astype(np.float32)
 	target = cv2.cvtColor(target, cv2.COLOR_BGR2LAB).astype(np.float32)
+	srcMean, srcStd = cv2.meanStdDev(source)
+	targetMean, targetStd = cv2.meanStdDev(target)
+	res = []
+	for i, channel in enumerate(cv2.split(target)):
+		channel -= targetMean[i]
+		channel *= srcStd[i] / targetStd[i]
+		channel += srcMean[i]
+		res.append(np.clip(channel, 0, 255))
+	result = cv2.merge(res)
+	return cv2.cvtColor(result.astype(np.uint8), cv2.COLOR_LAB2BGR)
 
-	# Calculating Mean and Standard Deviation of source and target images
-	(lMeanSrc, lStdSrc, aMeanSrc, aStdSrc, bMeanSrc, bStdSrc) = mean_std(source)
-	(lMeanTar, lStdTar, aMeanTar, aStdTar, bMeanTar, bStdTar) = mean_std(target)
-
-	# Subtracting means of target image from target channels
-	l, a, b = cv2.split(target)
-	l -= lMeanTar
-	a -= aMeanTar
-	b -= bMeanTar
-
-	# Scaling target channels
-	# By ratio of (stdTarget/stdSource)*(Target Channel)
-	l *= lStdTar / lStdSrc
-	a *= aStdTar / aStdSrc
-	b *= bStdTar / bStdSrc
-
-	# Adding Source Mean to Resulting channels
-	l += lMeanSrc
-	a += aMeanSrc
-	b += bMeanSrc
-
-	# Clip values which fall outside range[0,255]
-	l = np.clip(l, 0, 255)
-	a = np.clip(a, 0, 255)
-	b = np.clip(b, 0, 255)
-
-	# Merge channels together -> Convert to RGB color space
-	result = cv2.merge([l, a, b])
-	result = cv2.cvtColor(result.astype(np.uint8), cv2.COLOR_LAB2BGR)
-
-	return result
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
-	parser.add_argument("--source","-s",required=True,help="Path of image from whose color texture is being used")
-	parser.add_argument("--target","-t",required=True,help="Path of image on color texture is being applied")
+	parser.add_argument(
+		"--source",
+		"-s",
+		required=True,
+		help="Path of image from whose color texture is being used",
+	)
+	parser.add_argument(
+		"--target",
+		"-t",
+		required=True,
+		help="Path of image on color texture is being applied",
+	)
+	parser.add_argument(
+		"--result", "-r", default="result.jpg", help="Result file name"
+	)
 	args = vars(parser.parse_args())
+
 	source = cv2.imread(args["source"])
 	target = cv2.imread(args["target"])
-	result = color_transfer(source,target)
-	cv2.imwrite("result.jpg",result)
+	result = color_transfer(source, target)
+	cv2.imwrite(args["result"], result)
--- readme.md
+++ readme.md
@@ -5,7 +5,15 @@ Image color transfer is a technique of transferring color texture of one image t
 # Description
 
 ## Lαβ color space
+Lαβ color space is a **3-axis color system** 
 
+<img src = "lab-color-space.png" width = 400>
+
+* L represents lightness and α and β for the color dimensions. .
+* Lαβ is the most exact representation or most **natural representation** of colors just like we humans see them.
+
+This algorithm of color transfer was proposed in 
+[Color Transfer between images]("https://www.cs.tau.ac.il/~turkel/imagepapers/ColorTransfer.pdf").
 
 ## Getting Started
 
@@ -27,15 +35,23 @@ python color_transfer.py
 --source or -s : Path to source image (Image to be used for transferring color)
 
 --target or -t : Path to target image (Image on which color is transferred)
+
+--result or -r : Path to resulting image
 ```
 
 ## Results
 Images are taken from google images
 
 <div align = "left">
-<img align = "center`" src = "images/morning.jpg" width = 250 height = 230>
-<img align = "center`" src = "images/sunset.jpg" width = 250 height = 230>
-<img align = "center`" src = "images/result.jpg" width = 250 height = 230>
+<img align = "center`" src = "source/evening.jpg" width = 250 height = 230>
+<img align = "center`" src = "target/colosseum.jpg" width = 250 height = 230>
+<img align = "center`" src = "result/colosseum_evening.jpg" width = 250 height = 230>
+</div>
+
+<div align = "left">
+<img align = "center`" src = "target\beach.jpg" width = 250 height = 230>
+<img align = "center`" src = "target\evening.jpg" width = 250 height = 230>
+<img align = "center`" src = "result/evening_morning.jpg" width = 250 height = 230>
 </div>
 
 ## Contributors
@@ -48,6 +64,4 @@ This project is licensed under the MIT License - see the LICENSE.md file for det
 
 ## Acknowledgments
 
-Inspiration, code snippets, etc.
-* [pyimagesearch](https://pyimagesearch.com/2014/06/30/super-fast-color-transfer-images/)
-* Color Transfer between Images by Reinhard et al, 2001
+* [Color Transfer between images]("https://www.cs.tau.ac.il/~turkel/imagepapers/ColorTransfer.pdf")
